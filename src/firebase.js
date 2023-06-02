dba17700ebdbccae4958a2298b9db75d6a578e0d
Smit Jiwani <78549024+smitjiwani@users.noreply.github.com>
2023-06-02 13:00:42 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCWVqbRzot34P-XAhV75NyoPcIVycvcmwE",
-    authDomain: "linkedin-clone-639f1.firebaseapp.com",
-    projectId: "linkedin-clone-639f1",
-    storageBucket: "linkedin-clone-639f1.appspot.com",
-    messagingSenderId: "229454112654",
-    appId: "1:229454112654:web:235ea0afa031ca018e66df",
-    measurementId: "G-WL37Z81NT9"
+    // Add firbase config here.  
   };
 
 
