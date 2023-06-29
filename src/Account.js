f0430041e969a501759c2f707c8f4af1646c8b7e
smit <smitjiwani44@gmail.com>
2023-06-29 12:45:15 +0530

    This adds account and settings page

---
+++ src/Account.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectUser } from "./features/counter/userSlice";
+import Login from './Login';
+import Header from './Header';
+
+function Account() {
+    const user = useSelector(selectUser);
+  return (
+    <div>
+      <div>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+
+            <span>Account page</span>
+          </div>
+        </>
+      )}
+
+    </div>
+    </div>
+  )
+}
+
+export default Account
