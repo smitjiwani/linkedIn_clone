2a93607c65d7477e834d89ed99d0a7a1f56b84c2
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-01-15 02:31:28 +0530

    Network

--- src/Network.js
+++ src/Network.js
@@ -3,21 +3,38 @@ import Header from './Header'
 import { useSelector } from 'react-redux';
 import { selectUser } from './features/counter/userSlice';
 import Login from './Login';
+import './Network.css'
+import Card from './Card.jsx'
+import Sidebar from "./Sidebar";
+import People from './people.jsx';
+
 
 function Network() {
   const user = useSelector(selectUser);
   return (
-    <div>
-      {!user ? (
+    <div >
+
+
+
+
+
+{!user ? (
         <Login />
       ) : (
-        <>
-          <Header />
-          <div className="app__body">
 
-            <span>Network page</span>
-          </div>
-        </>
+
+
+        <div>
+        <Header />
+        <div className='coll'>
+          
+          <Sidebar />
+         <People></People>
+        </div>
+        </div>
+      
+      
+       
       )}
 
     </div>
